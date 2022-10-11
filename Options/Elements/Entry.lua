11aa5a90fd8d7eacad44115fbcc856aeee48856f
Skamer <Skamer@users.noreply.github.com>
2022-10-11 15:01:24 +0200

    Add Entry and add get/set method for FauxScrollBox
    ref #20

---
+++ Options/Elements/Entry.lua
@@ -0,0 +1,395 @@
+-- ========================================================================= --
+--                              SylingTracker                                --
+--           https://www.curseforge.com/wow/addons/sylingtracker             --
+--                                                                           --
+--                               Repository:                                 --
+--                   https://github.com/Skamer/SylingTracker                 --
+--                                                                           --
+-- ========================================================================= --
+Syling              "SylingTracker_Options.Elements.Entry"                   ""
+-- ========================================================================= --
+struct "SUI.EntryData" {
+    { name = "text",          type = String },
+    { name = "value",         type = Any },
+    { name = "widgetClass",   type = IEntry },
+    { name = "properties",    type = Table},
+}
+
+interface "SUI.IEntry" (function(_ENV)
+  require "Frame"
+  -----------------------------------------------------------------------------
+  --                               Methods                                   --
+  -----------------------------------------------------------------------------
+  __Arguments__ { SUI.EntryData}
+  function SetupFromEntryData(self, data) 
+    self:SetEntryData(data)
+    self:InitProperties(data)
+  end
+
+  function GetEntryData(self) 
+    return self.EntryData 
+  end
+  
+  __Arguments__{  SUI.EntryData/nil}
+  function SetEntryData(self, data) 
+    self.EntryData = data 
+  end 
+
+  __Arguments__ { SUI.EntryData}
+  function InitProperties(self, data)
+    if data.properties then 
+      for property, value in pairs(data.properties) do 
+        self[property] = value
+      end
+    end 
+  end
+  -----------------------------------------------------------------------------
+  --                               Properties                                --
+  -----------------------------------------------------------------------------
+  property "EntryData" {
+    type = SUI.EntryData
+  }
+end)
+
+
+interface "SUI.IButtonEntry" (function(_ENV)
+  require "Button" extend "SUI.IEntry"
+end)
+
+
+__Widget__()
+class "SUI.EntryButton" (function(_ENV)
+  inherit "Button" extend "SUI.IButtonEntry"
+  -----------------------------------------------------------------------------
+  --                               Methods                                   --
+  -----------------------------------------------------------------------------
+  __Arguments__{ SUI.EntryData }
+  function SetupFromEntryData(self, data)
+    super.SetupFromEntryData(self, data)
+
+    Style[self].SelectionDetails.SelectionName.text = data.text or ""
+  end 
+
+  function RefreshState(self)
+    local highlightBackground = self:GetChild("HighlightBackground")
+    local selectionName = self:GetChild("SelectionDetails"):GetChild("SelectionName")
+    local fontColor = nil 
+
+    if self.selected then 
+      highlightBackground:SetAlpha(0)
+      fontColor = NORMAL_FONT_COLOR
+    else
+      if self.mouseover then 
+        highlightBackground:SetAlpha(0.15)
+      else 
+         highlightBackground:SetAlpha(0)
+      end
+
+      fontColor = GRAY_FONT_COLOR
+    end
+    
+    selectionName:SetTextColor(fontColor:GetRGB())
+  end
+
+  function OnRelease(self)
+    self.selected = nil 
+    self.mouseover = nil
+  end
+  -----------------------------------------------------------------------------
+  --                               Properties                                --
+  -----------------------------------------------------------------------------
+  property "Selected" {
+    type = Boolean,
+    default = false,
+    handler = function(self, new) self:RefreshState() end 
+  }
+
+  property "mouseover" {
+    type = Boolean,
+    default = false,
+    handler = function(self, new) self:RefreshState() end 
+  }
+  -----------------------------------------------------------------------------
+  --                            Constructors                                 --
+  -----------------------------------------------------------------------------
+  __Template__{
+    HighlightBackground = Frame,
+    SelectionDetails    = Frame,
+    {
+      SelectionDetails = {
+        SelectionName = FontString
+      }
+    }
+  }
+  function __ctor(self)
+    self.OnEnter = self.OnEnter + function() self.mouseover = true end 
+    self.OnLeave = self.OnLeave + function() self.mouseover = false end
+    
+    self:RefreshState()
+  end
+end)
+
+
+
+--- The interface adds the properties for holding the entries data
+interface "SUI.IEntryProvider" (function(_ENV)
+  -----------------------------------------------------------------------------
+  --                               Properties                                --
+  -----------------------------------------------------------------------------
+  property "EntriesData" {
+    set = false,
+    default = function() return Array[SUI.EntryData]() end 
+  }
+
+  property "DefaultEntryClass" {
+    type = -SUI.IEntry,
+    default = SUI.EntryButton
+  }
+end)
+
+--- Similar to EntryProvider excepted it doesn't hold entries data, only keep 
+--- a weak reference to them
+interface "SUI.IProxyEntryProvider" (function(_ENV)
+  -----------------------------------------------------------------------------
+  --                               Properties                                --
+  -----------------------------------------------------------------------------
+  --- Important: In case where this value is unexpected become nil, you Should
+  --- check if the data source is still avalaible.
+  __Set__(PropertySet.Weak)
+  property "EntriesData" {
+    type    = Array[SUI.EntryData]
+  }
+
+  property "DefaultEntryClass" {
+    type    = -SUI.IEntry,
+    default = SUI.EntryButton
+  }
+end)
+
+
+__Widget__()
+class "SUI.GridEntriesFauxScrollBox" (function(_ENV)
+  inherit "SUI.FauxScrollBox" extend "SUI.IProxyEntryProvider"
+  -----------------------------------------------------------------------------
+  --                               Events                                    --
+  -----------------------------------------------------------------------------
+  event "OnEntrySelected"
+  -----------------------------------------------------------------------------
+  --                               Handlers                                  --
+  -----------------------------------------------------------------------------
+  local function OnEntryClick(self, entry)
+    self:OnEntrySelected(entry)
+  end
+
+  local function OnOffsetChanged(self, newOffset, previousOffset)
+    self:Refresh()
+  end
+  -----------------------------------------------------------------------------
+  --                               Methods                                   --
+  -----------------------------------------------------------------------------
+  __Arguments__ { Number, -SUI.IEntry}
+  function AcquireEntry(self, index, entryClass)
+    local entry = entryClass.Acquire()
+    entry:SetParent(self:GetScrollContent())
+
+    -- If the Entry is a button, register onClick
+    if Class.IsObjectType(entry, SUI.IButtonEntry) then 
+      entry.OnClick = entry.OnClick + self.OnEntryClick
+
+    end
+
+    self.EntryFrames[index] = entry
+
+    return entry
+  end
+
+  function ReleaseEntries(self)
+    for index, entry in pairs(self.EntryFrames) do 
+      -- if the entry is a button, remove onClick handler 
+      if Class.IsObjectType(entry, SUI.IButtonEntry) then 
+        entry.OnClick = entry.OnClick - self.OnEntryClick
+      end
+
+      entry:Release()
+      self.EntryFrames[index] = nil
+    end
+  end
+
+  function GetEntryIndex(self, row, column)
+    return (row - 1) * self.ColumnCount + column
+  end
+
+  function Refresh(self)
+    --- It's important to check if there are data as this can become nil if 
+    --- the data source are no longer avalaible.
+    if not self.EntriesData then 
+      return 
+    end
+
+    self:ReleaseEntries()
+
+    local scrollBar = self:GetScrollBar()
+
+    self.RowCount = ceil(self.EntriesData.Count / self.ColumnCount)
+
+    local step = 1 / (self.RowCount - self.DisplayedRowCount)
+    local visibleExtentPercent = Clamp(self.DisplayedRowCount /self.RowCount, 0, 1)
+    scrollBar:SetScrollStepPercentage(step)
+    scrollBar:SetVisibleExtentPercentage(visibleExtentPercent)
+
+    for rowIndex = 1, self.DisplayedRowCount do 
+      for columnIndex = 1, self.ColumnCount do 
+        local entryFrameIndex = self:GetEntryIndex(rowIndex, columnIndex)
+        local entryIndex = self:GetEntryIndex(rowIndex + self:GetOffset(), columnIndex)
+        local entryData = self.EntriesData[entryIndex]
+
+        if entryData then 
+          local entryClass = entryData.widgetClass or self.DefaultEntryClass
+          local entry = self:AcquireEntry(entryFrameIndex, entryClass)
+          entry:SetupFromEntryData(entryData)
+
+          Style[entry].height = self.RowHeight
+
+
+          if self.EntryExtendWidth and self.ColumnCount == 1 then 
+            entry:SetPoint("RIGHT")
+          else 
+            Style[entry].width  = self.ColumnWidth
+          end
+
+          entry:SetPoint("TOPLEFT", (columnIndex - 1) * self.ColumnWidth, -((rowIndex - 1) * self.RowHeight))
+        end
+      end
+    end
+  end
+  -----------------------------------------------------------------------------
+  --                               Properties                                --
+  -----------------------------------------------------------------------------
+  property "EntryFrames" {
+    set = false,
+    default = function() return Toolset.newtable(false, true) end
+  }
+
+  property "ColumnCount" {
+    type = Number,
+    default = 1
+  }
+
+  property "ColumnWidth" {
+    type = Number,
+    default = 75
+  }
+
+  --- Say if the entry will take the width of it's parent
+  --- this mode is only avalaible if ColumnCount is '1'
+  property "EntryExtendWidth" {
+    type = Boolean,
+    default = true
+  }
+
+  property "TestData" {
+    type = Number,
+    default = 100
+  }
+  -----------------------------------------------------------------------------
+  --                            Constructors                                 --
+  -----------------------------------------------------------------------------
+  __Template__{}
+  function __ctor(self)
+    self.OnEntryClick = function(entry)
+      OnEntryClick(self, entry)
+    end
+
+    self.OnOffsetChanged = self.OnOffsetChanged + OnOffsetChanged
+  end
+end)
+-------------------------------------------------------------------------------
+--                                Styles                                     --
+-------------------------------------------------------------------------------
+Style.UpdateSkin("Default", {
+  [SUI.EntryButton] = {
+    size = Size(250, 20),
+    SelectionDetails = {
+      location = {
+        Anchor("TOP"),
+        Anchor("LEFT", 14, 0),
+        Anchor("RIGHT"),
+        Anchor("BOTTOM")
+      },
+
+      SelectionName = {
+        setAllPoints = true, 
+        fontObject = GameFontNormal,
+        text = "Entry 1",
+        justifyH = "LEFT",
+        maxLines = 1,
+        drawLayer = "OVERLAY",
+        subLevel = 1
+      }
+    },
+
+    HighlightBackground = {
+      setAllPoints = true, 
+      alpha = 0,
+      
+      LeftBGTexture = {
+        atlas = AtlasType("charactercreate-customize-dropdown-linemouseover-side", true),
+        drawLayer = "BACKGROUND",
+        location = {
+          Anchor("TOPLEFT")
+        }
+      },
+      RightBGTexture = {
+        atlas = AtlasType("charactercreate-customize-dropdown-linemouseover-side", true),
+        drawLayer = "BACKGROUND",
+        texCoords = { left = 1, right = 0, top = 0, bottom = 1},
+        location = {
+          Anchor("TOPRIGHT")
+        }
+      },
+      MiddleBGTexture = {
+        atlas = AtlasType("charactercreate-customize-dropdown-linemouseover-middle", true),
+        drawLayer = "BACKGROUND",
+        location = {
+          Anchor("TOPLEFT", 0, 0, "LeftBGTexture", "TOPRIGHT"),
+          Anchor("BOTTOMRIGHT", 0, 0, "RightBGTexture", "BOTTOMLEFT")
+        }
+      }
+    }
+  },
+  [SUI.GridEntriesFauxScrollBox] = {
+    size = Size(200, 200),
+    ScrollBar = {
+      location = {
+        Anchor("TOPRIGHT"),
+        Anchor("BOTTOMRIGHT")
+      }
+    },
+    ScrollContent = {
+      location = {
+        Anchor("TOPLEFT"),
+        Anchor("BOTTOMRIGHT", -5, 0, "ScrollBar", "BOTTOMLEFT")
+      }
+    }
+  }
+})
+
+--   local entriesData = Array[SUI.EntryData]()
+
+-- function OnLoad(self)
+--   local widget = SUI.GridEntriesFauxScrollBox.Acquire(true, UIParent)
+--   widget:SetPoint("CENTER")
+
+--     -- local entriesData = Array[SUI.EntryData]()
+
+--   for i = 1, 50 do 
+--     if i == 1 then 
+--       entriesData:Insert({text = "Entry"..i, properties = { selected = true}})
+--     else 
+--       entriesData:Insert({text = "Entry"..i})
+--     end
+--   end
+
+--   widget.EntriesData = entriesData
+--   widget:Refresh()
+-- end
--- Options/Elements/ScrollBox.lua
+++ Options/Elements/ScrollBox.lua
@@ -82,11 +82,119 @@ class "SUI.ScrollBox" (function(_ENV)
     end
   end
 end)
+
+__Widget__()
+class "SUI.FauxScrollFrame" (function(_ENV)
+  inherit "Frame"
+  -----------------------------------------------------------------------------
+  --                               Methods                                   --
+  -----------------------------------------------------------------------------
+  function GetScrollBar(self)
+    return self:GetChild("ScrollBar")
+  end
+
+  function GetScrollContent(self)
+    return self:GetChild("ScrollContent")
+  end
+  -----------------------------------------------------------------------------
+  --                            Constructors                                 --
+  -----------------------------------------------------------------------------
+  __Template__{
+    ScrollBar = SUI.ScrollBar,
+    ScrollContent = Frame
+  }
+  function __ctor(self) end 
+end)
+
+
+__Widget__()
+class "SUI.FauxScrollBox" (function(_ENV)
+  inherit "SUI.FauxScrollFrame"
+  -----------------------------------------------------------------------------
+  --                               Handlers                                  --
+  -----------------------------------------------------------------------------
+  local function OnMouseWheel(self, direction)
+    local scrollBar = self:GetScrollBar()
+    scrollBar:OnMouseWheel(direction)
+  end
+
+  local function OnScroll(self, value)
+    local offset = max(0, (self.RowCount - self.DisplayedRowCount) * value)
+    self.Offset = offset
+  end
+  -----------------------------------------------------------------------------
+  --                               Methods                                   --
+  -----------------------------------------------------------------------------
+  function GetOffset(self)
+    return self.Offset
+  end
+
+  __Arguments__ { Number/nil }
+  function SetDisplayedRowCount(self, displayedRowCount)
+    self.DisplayedRowCount = displayedRowCount
+  end
+
+  function GetDisplayedRowCount(self)
+    return self.DisplayedRowCount
+  end
+
+  __Arguments__ { Number/nil }
+  function SetRowCount(self, rowCount)
+    self.RowCount = rowCount
+  end
+
+  function GetRowCount(self)
+    return self.RowCount
+  end  
+  -----------------------------------------------------------------------------
+  --                               Properties                                --
+  -----------------------------------------------------------------------------
+  property "Offset" {
+    type      = Number,
+    default   = 0,
+    event     = "OnOffsetChanged"
+  }
+
+  property "DisplayedRowCount" {
+    type      = Number,
+    default   = 10
+  }
+
+  property "RowCount" {
+    type      = Number,
+    default   = 0
+  }
+
+  property "RowHeight" {
+    type      = Number,
+    default   = 20
+  }
+  -----------------------------------------------------------------------------
+  --                            Constructors                                 --
+  -----------------------------------------------------------------------------
+  __Template__{}
+  function __ctor(self)
+    self.OnMouseWheel = self.OnMouseWheel + OnMouseWheel
+
+    local scrollBar = self:GetScrollBar()
+    scrollBar.OnScroll = scrollBar.OnScroll + function(_, value)
+      OnScroll(self, value)
+    end
+  end
+end)
 -------------------------------------------------------------------------------
 --                                Styles                                     --
 -------------------------------------------------------------------------------
 Style.UpdateSkin("Default", {
   [SUI.ScrollFrame] = {
+    ScrollBar = {
+      location = {
+        Anchor("TOPLEFT", 8, -16, nil, "TOPRIGHT"),
+        Anchor("BOTTOMLEFT", 8, 16, nil, "BOTTOMRIGHT")
+      }      
+    }
+  },
+  [SUI.FauxScrollFrame] = {
     ScrollBar = {
       location = {
         Anchor("TOPLEFT", 8, -16, nil, "TOPRIGHT"),
